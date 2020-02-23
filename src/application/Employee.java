4a336529be5bbf667db4911343fe122de2e01ee4
David Almeida <davidalmeidadrive@gmail.com>
2020-02-23 01:10:43 -0300

    first commit

---
+++ src/application/Employee.java
@@ -0,0 +1,32 @@
+package application;
+
+public class Employee {
+	
+	private String name;
+	private Integer hours;
+	private Double valuePerHour;
+	
+	
+	public Employee(String name, Integer hours, Double valuePerHour) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Integer getHours() {
+		return hours;
+	}
+	
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+	
+	
+	
+
+}
